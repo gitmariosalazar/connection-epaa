36ea70ec8facc45daa33bf8b5d040c0be9ba1379
Mario Salazar <mariosalazar.ms.10@gmail.com>
2026-06-04 11:51:55 -0500

    feat: upload new features

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { AppConnectionModulesUsingPostgreSQL } from './factory/postgresql/module
 import { AppConnectionModulesUsingMySQL } from './factory/mysql/modules-using-mysql.module';
 import { environments } from './settings/environments/environments';
 import { DatabasePersistenceModule } from './shared/connections/database/database-persistence.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import * as path from 'path';
 
 const connectionModules =
   environments.DATABASE_TYPE === 'mysql'
@@ -15,16 +13,7 @@ const connectionModules =
     : AppConnectionModulesUsingPostgreSQL;
 
 @Module({
-  imports: [
-    HomeModule,
-    connectionModules,
-    DatabasePersistenceModule,
-    ServeStaticModule.forRoot({
-      rootPath: environments.CONNECTION_DOCUMENTS_UPLOAD_ROOT,
-      serveRoot: '/uploads',
-      serveStaticOptions: { index: false, redirect: false },
-    }),
-  ],
+  imports: [HomeModule, connectionModules, DatabasePersistenceModule],
   controllers: [AppController],
   providers: [AppService],
 })
